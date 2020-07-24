2fa45fb92750eb2dc32ed8905010756c30609aef
vinaywilliams <vinaywilliams00@gmail.com>
2020-07-24 18:05:42 +0100

    updating

--- src/main/python/Shrike V1/shrike_1.py
+++ src/main/python/Shrike V1/shrike_1.py
@@ -4,10 +4,8 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scipy import integrate
-import statistics
 from scipy.stats import linregress
 from scipy.integrate import odeint
-import os
 try:
     import CoolProp.CoolProp
     from CoolProp.CoolProp import PropsSI
@@ -19,11 +17,12 @@ except ImportError:
 t_s = True
 
 "INPUTS"
-engine_name = "P1F"
-a_l = .3
+steppeddata_filename = "./data/test.csv"
+otherdata_filename = "./data/data.txt"
+
 e_d = 0.1524 # Airframe internal diameter
 i_d = 0.147 #Internal Motor Diameter
-t_l = 0.8 # Thrust Chamber Length
+t_l = 1 # Thrust Chamber Length
 # Environment
 g = 9.81    #"Gravitational Acceleration, m/s^2"
 Pa = 101325 #"Ambient Pressure, Pascal"
@@ -33,7 +32,7 @@ Ta = 300    #"Ambient Temperature, K
 n_cstar = 0.9     #"Combustion Efficiency"
 n_cf = 0.885           #"Cf Efficiency"
 k = 1.114           #"Ratio of Specific Heats with respect to combustion products"
-Dint = 0.03          #"Initial Paraffin Grain Diameter, m"
+Dint = 0.02          #"Initial Paraffin Grain Diameter, m"
 Dchamber = i_d     #"Chamber Diameter, m"
 pcomb = 900         #"Paraffin's density, kg/m^3"
 Vol = t_l * np.pi * (i_d/2) ** 2     #"Free chamber volume, m^3"
@@ -41,7 +40,7 @@ M = 26.912            #"Molar mass of combustion products, kg/kmol"
 To = 3347             #"Chamber temperature, K"
 Dthroat = 26*10**-3   #"Throat Diameter, m"
 Dexit =41*10**-3 #"Exit Diameter, m"
-Lgrain = 0.5    #"Paraffin Grain Lenght, m"
+Lgrain = 0.75    #"Paraffin Grain Lenght, m"
 
 # Oxidiser Tank
 Cd = 0.65     #"Injector's Discharge Coefficient"
@@ -49,31 +48,8 @@ IA = 0.0002  #"Injector Area, m^2"
 dox = i_d   # Diameter of oxidiser tank (m)
 
 Cd_Vent = 0.65       # Vent's Discharge Coefficient
-i_ox_mass = 16 # Initial oxidiser mass, kg
-fuel_mass = (((Dchamber/2)*(Dchamber/2)) - ((Dint/2)*(Dint/2)))*3.14*Lgrain*pcomb
-length_tank = 1.4# Length of oxidiser tank, m
-
-"STORAGE PATH SETUP"
-try:
-    os.mkdir("./"+engine_name)
-except:
-    print("Overwriting old data in "+engine_name)
-
-try:
-    os.mkdir("./"+engine_name+"/data")
-except:
-    pass
-
-try:
-    os.mkdir("./"+engine_name+"/images")
-except:
-    pass
-
-image_path = "./"+engine_name+"/images"
-data_path = "./"+engine_name+"/data"
-steppeddata_filename = data_path+"/discreet.csv"
-otherdata_filename = data_path+"/data.txt"
-thrustcurve = data_path+"/"+engine_name+".eng"
+i_ox_mass = 20 # Initial oxidiser mass, kg
+length_tank = 1.7# Length of oxidiser tank, m
 
 "COMPUTED PARAMETERS"
 
@@ -253,8 +229,8 @@ while chamber_pressure[i] > 1.1*Pa:
     grain_radius.append(grain_radius[i-1])
     tank_pressure.append(tank_pressure[i-1])
     if Pa < chamber_pressure[i]:
-        cf.append(((((2*k**2)/(k-1))*(2/(k+1))**((k+1)/(k-1))*(1-(Pa/chamber_pressure[i])**((k-1)/k)))**0.5)[0])
-        thrust.append((n_cf*cf[i]*(np.pi*(Dthroat**2)/4)*chamber_pressure[i])[0])
+        cf.append((((2*k**2)/(k-1))*(2/(k+1))**((k+1)/(k-1))*(1-(Pa/chamber_pressure[i])**((k-1)/k)))**0.5)
+        thrust.append(n_cf*cf[i]*(np.pi*(Dthroat**2)/4)*chamber_pressure[i])
     else:
         cf.append(0)
         thrust.append(0)
@@ -282,7 +258,7 @@ fig.suptitle('Thrust Curve', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Thrust (N)', fontsize=10)
 plt.plot(t,thrust[0:len(thrust)-1])
-plt.savefig(image_path+"/thrustcurve.png")
+plt.savefig("./images/thrustcurve.png")
 
 # Plot Pressure Curve
 fig2 = plt.figure()
@@ -290,7 +266,7 @@ fig2.suptitle('Pressure Curve', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Pressure (Pa)', fontsize=10)
 plt.plot(t,chamber_pressure[0:len(chamber_pressure)-1])
-plt.savefig(image_path+"/pressurecurve.png")
+plt.savefig("./images/pressurecurve.png")
 
 # Plot Tank Pressure  Curve
 fig3 = plt.figure()
@@ -298,7 +274,7 @@ fig3.suptitle('Tank Pressure ', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Pressure (Pa)', fontsize=10)
 plt.plot(t,tank_pressure[0:len(tank_pressure)-1])
-plt.savefig(image_path+"/tankpressure.png")
+plt.savefig("./images/tankpressure.png")
 
 # Plot Tank Temperature Curve
 fig4 = plt.figure()
@@ -306,7 +282,7 @@ fig4.suptitle('Tank Temperature', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Temperature (K)', fontsize=10)
 plt.plot(t,temperature_tank[0:len(temperature_tank)-1])
-plt.savefig(image_path+"/tanktemperature.png")
+plt.savefig("./images/tanktemperature.png")
 
 # Plot Grain Radius Curve
 fig5 = plt.figure()
@@ -314,7 +290,7 @@ fig5.suptitle('Grain Radius ', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Radius (meters)', fontsize=10)
 plt.plot(t,grain_radius[0:len(grain_radius)-1])
-plt.savefig(image_path+"/grainradius.png")
+plt.savefig("./images/grainradius.png")
 
 # Plot Quality Curve
 fig6 = plt.figure()
@@ -322,7 +298,7 @@ fig6.suptitle('Quality', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Phase Ratio', fontsize=10)
 plt.plot(t,quality[0:len(quality)-1])
-plt.savefig(image_path+"/quality.png")
+plt.savefig("./images/quality.png")
 
 # Plot Mass Nitrous Oxide Curve
 fig7 = plt.figure()
@@ -330,7 +306,7 @@ fig7.suptitle('Mass N2O', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Mass N2O (kg)', fontsize=10)
 plt.plot(t,mass_nitrous[0:len(mass_nitrous)-1])
-plt.savefig(image_path+"/n2omass.png")
+plt.savefig("./images/n2omass.png")
 
 # Plot Entropy Curve
 fig8 = plt.figure()
@@ -338,7 +314,7 @@ fig8.suptitle('Entropy', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Entropy', fontsize=10)
 plt.plot(t,total_entropy[0:len(total_entropy)-1])
-plt.savefig(image_path+"/entropy.png")
+plt.savefig("./images/entropy.png")
 
 # Plot Mass Liquid N2O Curve
 fig9 = plt.figure()
@@ -346,7 +322,7 @@ fig9.suptitle('Mass Liquid N2O', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Mass Liquid N2O', fontsize=10)
 plt.plot(t,mass_liquid_nitrous[0:len(mass_liquid_nitrous)-1])
-plt.savefig(image_path+"/massliquidn20.png")
+plt.savefig("./images/massliquidn20.png")
 
 # Plot Mass Vapur N2O Curve
 fig10 = plt.figure()
@@ -354,7 +330,7 @@ fig10.suptitle('Mass Vapour N2O', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Mass Vapour N2O', fontsize=10)
 plt.plot(t,mass_vapour_nitrous[0:len(mass_vapour_nitrous)-1])
-plt.savefig(image_path+"/massvapourn20.png")
+plt.savefig("./images/massvapourn20.png")
 
 # Plot Chamber Pressure Curve
 fig11 = plt.figure()
@@ -362,7 +338,7 @@ fig11.suptitle('Chamber Pressure', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Chamber Pressure', fontsize=10)
 plt.plot(t,chamber_pressure[0:len(chamber_pressure)-1])
-plt.savefig(image_path+"/chamberpressure.png")
+plt.savefig("./images/chamberpressure.png")
 
 # Plot N2O Mass Flow Rate Curve
 fig12 = plt.figure()
@@ -370,7 +346,7 @@ fig12.suptitle('Mass Flow Rate', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('N2O Mass Flow Rate', fontsize=10)
 plt.plot(t,n2o_mass_flow_rate[0:len(chamber_pressure)-1])
-plt.savefig(image_path+"/n2omassflowrate.png")
+plt.savefig("./images/n2omassflowrate.png")
 
 # Plot Exhaust Mass Flow Rate Curve
 fig12 = plt.figure()
@@ -378,7 +354,7 @@ fig12.suptitle('Exhaust Mass Flow Rate', fontsize=12)
 plt.xlabel('Time (s)', fontsize=10)
 plt.ylabel('Exhaust Mass Flow Rate', fontsize=10)
 plt.plot(t,exhaust_mass_flow_rate[0:len(chamber_pressure)-1])
-plt.savefig(image_path+"/exhaustmassflowrate.png")
+plt.savefig("./images/exhaustmassflowrate.png")
 
 print("Figures Plotted....")
 
@@ -390,89 +366,34 @@ df.to_csv(steppeddata_filename, index = False)
 slope = linregress(t,mass_nitrous[0:len(quality)-1])[0]
 impulse = np.trapz(thrust[0:len(thrust)-1], x = t)
 
-if impulse >= 0 and impulse <= 0.3125:
-    motor_class = "micro"
-elif impulse >= 0.3126 and impulse <= 0.625:
-    motor_class = "1/4A"
-elif impulse >= 0.626 and impulse <= 1.25:
-    motor_class = "1/2A"
-elif impulse >= 1.26 and impulse <= 2.5:
-    motor_class = "A"
-elif impulse >= 2.51 and impulse <= 5.0:
-    motor_class = "B"
-elif impulse >= 5.01 and impulse <= 10.0:
-    motor_class = "C"
-elif impulse >= 10.01 and impulse <= 20.0:
-    motor_class = "D"
-elif impulse >= 20.01 and impulse <= 40.0:
-    motor_class = "E"
-elif impulse >= 40.01 and impulse <= 80.0:
-    motor_class = "F"
-elif impulse >= 80.01 and impulse <= 160.0:
-    motor_class = "G"
-elif impulse >= 160.1 and impulse <= 320.0:
-    motor_class = "H"
-elif impulse >= 320.01 and impulse <= 640.0:
-    motor_class = "I"
-elif impulse >= 640.01 and impulse <= 1280.0:
-    motor_class = "J"
-elif impulse >= 1280.01 and impulse <= 2560.0:
-    motor_class = "K"
-elif impulse >= 2560.01 and impulse <= 5120.0:
-    motor_class = "L"
-elif impulse >= 5120.01 and impulse <= 10240.0:
-    motor_class = "M"
-elif impulse >= 10240.01 and impulse <= 20480.0:
-    motor_class = "N"
-elif impulse >= 20480.0 and impulse <= 40960.0:
-    motor_class = "O"
-elif impulse >= 40960.01 and impulse <= 81920.0:
-    motor_class = "P"
-elif impulse >= 81920.01 and impulse <= 163840.0:
-    motor_class = "Q"
-elif impulse >= 163840.01 and impulse <= 327680.0:
-    motor_class = "R"
-elif impulse >= 327680.01 and impulse <= 655360.0:
-    motor_class = "S"
-
-#av_t = round(mean(thrust),0)
-
-
 with open(otherdata_filename, "w") as file:
-    file.write("Airframe Parameters:"+"\n")
-    file.write("Internal Diameter: "+str(e_d)+"\n")
+    file.write("Performance Stats: \n")
+    file.write("Airframe Parameters:")
+    file.write("Internal Diameter: "+str(e_d))
     file.write("Internal Motor Diameter: "+str(i_d)+"\n")
-    file.write("Operating Conditions:"+"\n")
-    file.write("Ambient Pressure: "+str(Pa)+"\n")
+    file.write("Operating Conditions:")
+    file.write("Ambient Pressure: "+str(Pa))
     file.write("Ambient Temperature: "+str(Ta)+"\n")
     file.write("Engine Specificiations: \n")
-    file.write("Combustion Efficiency: "+str(n_cstar)+"\n")
-    file.write("Initial Port Diameter: "+str(Dint)+"\n")
-    file.write("Chamber Temperature: "+str(To)+"\n")
-    file.write("Average N20 Mass Flow Rate: "+str(slope*-1)+"\n")
-    file.write("Final Grain Radius: "+str(round(grain_radius[len(mass_nitrous)-1],5))+"\n")
+    file.write("Combustion Efficiency: "+str(n_cstar))
+    file.write("Initial Port Diameter: "+str(Dint))
+    file.write("Chamber Temperature: "+str(To))
+    file.write("Average N20 Mass Flow Rate: "+str(slope*-1))
+    file.write("Final Grain Radius: "+str(round(grain_radius[len(mass_nitrous)-1],5)))
     file.write("Total Impulse: "+ str(impulse)+"\n")
-    file.write("Length of Grain: "+str(Lgrain)+"\n")
+    file.write("Length of Grain: "+str(Lgrain))
     file.write("Nozzle Parameters: \n")
-    file.write("Throat Diameter: "+str(Dthroat)+"\n")
-    file.write("Exit Diameter "+str(Dexit)+"\n")
+    file.write("Throat Diameter: "+str(Dthroat))
+    file.write("Exit Diameter "+str(Dexit))
     file.write("Oxidiser Tank Dimensions: \n")
-    file.write("Length of Tank: "+str(length_tank)+"\n")
-    file.write("Diameter of Tank: "+str(dox)+"\n")
+    file.write("Length of Tank: "+str(length_tank))
+    file.write("Diameter of Tank: "+str(dox))
     file.write("Volume of Tank: "+str(Vtank)+"\n")
     file.write("Oxidiser Paramters: \n")
-    file.write("Mass of Oxidiser: "+str(i_ox_mass)+"\n")
-    file.write("Motor Class:"+motor_class)
+    file.write("Mass of Oxidiser: "+str(i_ox_mass))
 
-with open(thrustcurve, "w") as file:
-    file.write(";\n")
-    file.write(";\n")
-    #file.write(motor_class+str(av_t)+" "+str(i_d)+" "+str(t_l+length_tank+a_l)+" 0 "+str(i_ox_mass+(i_ox_mass/3)+" "+str(i_ox_mass+(i_ox_mass/3)+18.3)+" "+engine_name))
-    file.write(engine_name+" 152 "+str((t_l+length_tank+a_l)*1000)+" 0 "+str(i_ox_mass+fuel_mass)+" "+str((i_ox_mass+fuel_mass)*2.5)+" Shrike\n")
-    for i in range(len(t)-1):
-        file.write(str(round(t[i],2))+"    "+str(thrust[i])+"\n")
 
 
 print("N20 Mass Flow Rate: "+str(slope))
 print("Final Grain Radius: "+str(round(grain_radius[len(mass_nitrous)-1],5)))
-print("Impulse: "+ str(round(impulse,5)))
+print("Impulse: "+ str(round(impulse[0],5)))
